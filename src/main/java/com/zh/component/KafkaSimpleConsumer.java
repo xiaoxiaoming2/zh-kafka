9aafbc42020ac23d3baa828a49d51209af4fabaa
24971 <24971@DESKTOP-LEQI8PJ>
2020-02-14 20:33:27 +0800

    x

--- src/main/java/com/zh/component/KafkaSimpleConsumer.java
+++ src/main/java/com/zh/component/KafkaSimpleConsumer.java
@@ -1,10 +1,6 @@
 package com.zh.component;
 
-import org.apache.kafka.clients.consumer.Consumer;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.support.KafkaHeaders;
-import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
 
 import lombok.extern.slf4j.Slf4j;
@@ -15,9 +11,8 @@ public class KafkaSimpleConsumer {
 
 // 简单消费者
 	@KafkaListener(topics = "test")
-	public void consumer1_1(ConsumerRecord<String, Object> record, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic,
-			Consumer consumer) {
-		System.out.println("消费者收到消息:" + record.value() + "; topic:" + topic);
+	public void consumer1_1(String message) {
+		System.out.println("消费者收到消息:" + message);
 		/*
 		 * 如果需要手工提交异步 consumer.commitSync(); 手工同步提交 consumer.commitAsync()
 		 */
