1b1933d1adc221f86961255b06e4843d60c1516d
24971 <24971@DESKTOP-LEQI8PJ>
2020-02-14 14:43:26 +0800

    x

--- src/main/java/com/zh/controller/NoticeController.java
+++ src/main/java/com/zh/controller/NoticeController.java
@@ -1,16 +1,22 @@
 package com.zh.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alibaba.fastjson.JSONObject;
+import com.zh.component.KafKaCustomrProducer;
 import com.zh.vo.NoticeVO;
 
 @RestController
 public class NoticeController {
-	@GetMapping
+	@Autowired
+	private KafKaCustomrProducer kafKaCustomrProducer;
+	
+	@GetMapping("test")
 	public String sendNotice() {
-		return null;
+		 kafKaCustomrProducer.sendMessage("test", "HelloWorld");
+		 return "";
 	}
 	
 	public static void main(String[] args) {
